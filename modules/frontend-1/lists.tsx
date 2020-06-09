3071f84f2966cec682331d24cb6db53a3f484dca
Kian Khoshnoodpour <kian.khoshnoodpour@ahold.com>
2020-06-09 16:46:59 +0200

    frontend-1 implementation

--- modules/frontend-1/lists.tsx
+++ modules/frontend-1/lists.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useListsQuery } from '../../codegen/_graphql';
+
+const List = styled.div`
+    padding: 8px;
+    margin: 0 0 24px;
+    border: 1px solid #eee;
+`;
 
 export const Lists: React.FC = () => {
-    return null;
+    const { loading, error, data } = useListsQuery();
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        return <p>Error: {error}</p>;
+    }
+
+    if (!data || !data.lists) {
+        return <p>Erorr: Unknown</p>;
+    }
+
+    return (
+        <>
+            {data.lists.map((list) => (
+                <List key={list.id}>{list.description}</List>
+            ))}
+        </>
+    );
 };
