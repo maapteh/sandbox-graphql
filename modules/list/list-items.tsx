fccb3f182d202763d6b6147a63759e1a82eba410
Kian Khoshnoodpour <kian.khoshnoodpour@ahold.com>
2020-06-16 16:55:00 +0200

    chapter 6 solution

--- modules/list/list-items.tsx
+++ modules/list/list-items.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useListItemsQuery } from '../../codegen/_graphql';
+import { useListItemsLazyQuery } from '../../codegen/_graphql';
 
 const Container = styled.div`
+    margin-top: 16px;
+`;
+
+const Item = styled.div`
     padding: 8px;
     margin: 24px;
     border: 1px solid #eee;
 `;
 
 export const ListItems: React.FC<{ id: number }> = ({ id }) => {
-    const { data } = useListItemsQuery({
-        ssr: false,
-        variables: {
-            id,
-        },
-    });
+    const [loadItems, { data }] = useListItemsLazyQuery();
 
     return (
-        <>
-            {data?.list?.items?.map((item) => {
-                return (
-                    <Container>
-                        {item.quantity}x {item.description}
-                    </Container>
-                );
-            })}
-        </>
+        <Container>
+            <button
+                onClick={() => {
+                    loadItems({
+                        variables: {
+                            id,
+                        },
+                    });
+                }}
+            >
+                Load Items
+            </button>
+            {data?.list?.items &&
+                data.list.items.length > 0 &&
+                data.list.items.map((item, index) => {
+                    return (
+                        <Item key={item.id || item.description || index}>
+                            {item.quantity}x {item.description}
+                        </Item>
+                    );
+                })}
+        </Container>
     );
 };
