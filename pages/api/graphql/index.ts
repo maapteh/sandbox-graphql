4b1a2fc4c4708b8d921415a253019cfd0e29cd91
maapteh <maarten@oudenniel.nl>
2020-06-17 06:39:43 +0200

    show option for app in https (when having hsts problems) prepare cachecontrol for mpth case

--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,6 +1,9 @@
 import { ApolloServer, gql, ApolloError } from 'apollo-server-micro';
+import responseCachePlugin from 'apollo-server-plugin-response-cache';
 import { typeDefs } from './schema';
 import { resolvers } from './resolvers';
+
+const Dataloader = require('dataloader');
 
 const apolloServer = new ApolloServer({
     typeDefs,
